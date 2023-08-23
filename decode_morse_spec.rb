89b91bec955eaa73dffa39d041377497437a3f4f
rbd3 <rabedesanaa@gmail.com>
2023-08-23 17:05:08 +0300

    Update function

--- decode_morse_spec.rb
+++ decode_morse_spec.rb
@@ -32,5 +32,20 @@ def decode_char(char)
   @single_char
 end
 
+def decode_word(word)
+  @one_word = word.split(' ')
+  @current_word = ''
+  @one_word.each do |chars|
+    decode_char(chars)
+    @current_word += decode_char(chars).upcase
+  end
+  print ' ' + @current_word
+end
+def decode_message(message)
+  @many_words = message.split('   ')
+  @many_words.each do |word|
+    @final = decode_word(word)
+  end
+end
 
 decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
